4818991cdad77bc1e4ba36908156ee09e6fef9f8
ayush-etailify <ayush@etailify.io>
2025-08-20 05:53:56 +0530

    build fix

--- packages/storefront-sdk/utils/token-manager.ts
+++ packages/storefront-sdk/utils/token-manager.ts
@@ -1,36 +1,60 @@
-import Cookies from "js-cookie";
-
 export class TokenManager {
-  private cookieOptions: Cookies.CookieAttributes;
+  private isDevelopment: boolean;
 
   constructor(isDevelopment = false) {
-    this.cookieOptions = {
-      secure: !isDevelopment,
-      sameSite: "strict",
-      path: "/",
-    };
+    this.isDevelopment = isDevelopment;
+  }
+
+  private setCookie(name: string, value: string): void {
+    if (this.isServer()) return;
+
+    const secure = !this.isDevelopment ? "; Secure" : "";
+    const sameSite = "; SameSite=Strict";
+    const path = "; Path=/";
+
+    document.cookie = `${name}=${value}${path}${sameSite}${secure}`;
+  }
+
+  private getCookie(name: string): string | undefined {
+    if (this.isServer()) return undefined;
+
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(";");
+
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return undefined;
+  }
+
+  private removeCookie(name: string): void {
+    if (this.isServer()) return;
+
+    document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
   }
 
   setAccessToken(accessToken: string): void {
-    Cookies.set("accessToken", accessToken, this.cookieOptions);
+    this.setCookie("accessToken", accessToken);
   }
 
   setAuthTokens(accessToken: string, refreshToken: string): void {
     this.setAccessToken(accessToken);
-    Cookies.set("refreshToken", refreshToken, this.cookieOptions);
+    this.setCookie("refreshToken", refreshToken);
   }
 
   getAccessToken(): string | undefined {
-    return Cookies.get("accessToken");
+    return this.getCookie("accessToken");
   }
 
   getRefreshToken(): string | undefined {
-    return Cookies.get("refreshToken");
+    return this.getCookie("refreshToken");
   }
 
   removeAuthTokens(): void {
-    Cookies.remove("accessToken", { path: "/" });
-    Cookies.remove("refreshToken", { path: "/" });
+    this.removeCookie("accessToken");
+    this.removeCookie("refreshToken");
   }
 
   isServer(): boolean {
