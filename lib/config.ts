3b2a5151bb38a9d3e5704babc2258b01d1ef1220
Ayush Saha <ayush@etailify.io>
2025-09-29 20:40:01 +0530

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -3,5 +3,5 @@ import { EtailifyStorefrontSDK } from "@/packages/storefront-sdk";
 export const sdk = new EtailifyStorefrontSDK({
   baseUrl: "https://customerapis.staging.etailify.io",
   storeSlug: "444nomizo-1",
-  storeId: "c8a7211f-3933-446b-be4e-11517799bf9d",
+  storeId: "f635090f-adce-4bb8-8735-8debf7241f76",
 });
